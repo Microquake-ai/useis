0ca200c580e1e1f399396542f715090efd24765d
Jean-Philippe <jpmercier@pop-os.localdomain>
2023-01-15 10:29:34 -0500

    creating a client to extract data from a repository in github and adding the missing library

---
+++ useis/clients/test_data.py
@@ -0,0 +1,5 @@
+from git import Repo
+
+git_url = 'https://jeanphilippemercier/useis_test_data.git'
+
+repo.clone_from(git_url, )
---
+++ useis/examples/ai/ai_picker.py
@@ -0,0 +1,4 @@
+from useis.processors.picker import Picker
+from useis.clients.test_data import get_test_data
+
+picker = Picker()
