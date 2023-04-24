557cd17ff966a8fe63db0cd4ea949da4df835fe0
jpmercier <jpmercier01@gmail.com>
2023-04-24 12:33:02 -0400

    updating the classifier adding a few lines to automatically download the file

--- useis/examples/classifier/classifier.py
+++ useis/examples/classifier/classifier.py
@@ -6,6 +6,12 @@ base_directory = '.'
 project = 'test_project'
 network = 'test_network'
 
-event_classifier = classifier.EventClassifier(base_directory, project, network)
+event_classifier = classifier.Classifier(base_directory, project, network)
+
+reload(model)
+
+if event_classifier.event_classifier is None:
+    classifier_model = model.EventClassifier.load()
+    event_classifier.add_model(classifier_model, gpu=False)
 
 # event_classifier.add_model()
